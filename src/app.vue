12e08fbc68401022c17316f04b2a1f712948b4b1
jsbfec <jsbfec@jsbfecdeiMac.local>
2017-09-12 11:33:10 +0800

    scroll loading

--- src/app.vue
+++ src/app.vue
@@ -18,7 +18,7 @@
                 </f7-navbar>
                 <!-- Pages -->
                 <f7-pages>
-                    <f7-page>
+                    <f7-page infinite-scroll>
                         <!-- Material Theme Navbar -->
                         <f7-navbar v-if="$theme.material">
                             <f7-nav-left>
@@ -42,54 +42,14 @@
                             <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about"
                                           title="Dynamic Route"></f7-list-item>
                         </f7-list>
-                        <mediaItem :media="media"></mediaItem>
-                        <div class="msg-list" v-if="mediaData">
 
-                            <div class="msg-item" v-for="item in mediaData">
-                                <div class="msg-inner">
-                                    <div class="tit"><a class="tit-href" href="#">{{item.title}}</a></div>
-                                    <div class="user-info">
-                                        <a class="a-666" href="#"><span class="name">{{item.author}}</span><span>{{item.description}}</span></a>
-                                    </div>
-                                    <div class="ting-box clearfix">
-                                        <div class="item-img dinline-l"><a href="#"><img :src="item.imgurl"
-                                                                                         class="img-head"></a></div>
-                                        <div class="item-audio dinline-l">
-                                            <div class="btn-ting">
-                                                <div class="btn-ting-txt"><span>¥</span><span
-                                                        class="price">1</span><span>听一下</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="answer-info">
-                                        <span>答案价值 100元 </span><span>，400人听过</span><span>，200人觉得赞</span>
-                                    </div>
+                        <div class="page-content"  data-distance="150">
+                            <div class="msg-list" v-if="media">
+                                <div class="msg-item" v-for="item in media">
+                                    <mediaItem :media="item"></mediaItem>
                                 </div>
                             </div>
                         </div>
-
-                        <f7-list media-list>
-                            <f7-list-item
-                                    link="/home/"
-                                    media="<img src='http://localhost:63342/zhida/assets/img/head-img.jpg'>"
-                                    title="Item Title"
-                                    subtitle="Item Subtitle"
-                                    text="Text"
-                                    after="Read more"
-                            >
-                                <div slot="root-start">Root Start</div>
-                                <div slot="root">Root</div>
-                                <div slot="content-start">Content Start</div>
-                                <div slot="content">Content</div>
-                                <div slot="media-start">Media Start</div>
-                                <div slot="media">Media</div>
-                                <span slot="after-start">After Start</span>
-                                <span slot="after">After</span>
-                                <div slot="inner-start">Inner Start</div>
-                                <div slot="inner">Inner</div>
-                            </f7-list-item>
-                        </f7-list>
                     </f7-page>
                 </f7-pages>
             </f7-view>
@@ -99,32 +59,101 @@
 
 <script type="text/ecmascript-6">
     import mediaItem from 'components/mediaItem.vue';
+    const url = 'http://localhost:63342/zhida/data/medialist.json';
     export default {
         name: 'Index',
         data() {
             return {
-                mediaData: [],
-                media:''
+                media: ''
             }
         },
         created() {
-            const url = 'http://localhost:63342/zhida/data/medialist.json';
+
             let _this = this;
             window.Dom7.ajax({
                 dataType: 'json',
                 url: url,
                 success: function (resp) {
-                    _this.mediaData = resp.media;
-                    _this.media=resp.media;
-                    console.log(_this);
+
+                    _this.media = resp.media.slice(0,9);
+                    _this.scrollLoad();
                 },
                 error: function (xhr) {
                     console.log("Error on ajax call " + xhr);
                     window.f7.hideIndicator();
                 }
             });
+
         },
-        components:{
+        methods: {
+            scrollLoad(){
+                console.log('xxx');
+                let myApp = new Framework7();
+                let $$ = Dom7;
+                // Loading flag
+                let loading = false;
+                // Last loaded index
+                let lastIndex = $$('.msg-list .msg-item').length;
+                // Max items to load
+                let maxItems = 60;
+                // Append items per load
+                let itemsPerLoad = 20;
+                // Attach 'infinite' event handler
+                $$('.infinite-scroll').on('infinite', function () {
+                    // Exit, if loading in progress
+                    if (loading) return;
+                    // Set loading flag
+                    loading = true;
+                    // Emulate 1s loading
+
+                    if (lastIndex >= maxItems) {
+                        // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
+                        myApp.detachInfiniteScroll($$('.infinite-scroll'));
+                        // Remove preloader
+                        $$('.infinite-scroll-preloader').remove();
+                        return;
+                    }
+                    window.Dom7.ajax({
+                        dataType: 'json',
+                        url: url,
+                        success: function (resp) {
+                            console.log(resp);
+                            let media = resp.media.slice(lastIndex + 1,(lastIndex + itemsPerLoad)>maxItems?maxItems:(lastIndex + itemsPerLoad));
+                            console.log(media);
+                            let html = '';
+                            for (let i = 0; i <= itemsPerLoad; i++) {
+                                html += '<div class="msg-item" v-if="media">' +
+                                        '<div class="msg-inner">' +
+                                        '<div class="tit"><a class="tit-href" href="#">'+media[i].title+'</a></div>' +
+                                        '<div class="user-info">' +
+                                        '<a class="a-666" href="#"><span class="name">'+media[i].author+'</span><span>'+media[i].description+'</span></a>' +
+                                        '</div>' +
+                                        '<div class="ting-box clearfix">' +
+                                        '<div class="item-img dinline-l">' +
+                                        '<div class="paddingT"></div><a href="#"></a>' +
+                                        '<img src="'+media[i].imgurl+'" class="img-head"></div>' +
+                                        '<div class="item-audio dinline-l">' +
+                                        '<div class="btn-ting">' +
+                                        '<div class="btn-ting-txt"><span>¥</span><span class="price">1</span><span>听一下</span>' +
+                                        '</div> </div> </div> </div>' +
+                                        '<div class="answer-info">' +
+                                        '<span>答案价值 100元 </span><span>，400人听过</span><span>，200人觉得赞</span>' +
+                                        ' </div></div></div>';
+                            }
+                            console.log(html);
+                            $$('.msg-list').append(html);
+                            lastIndex = $$('.msg-list .msg-item').length;
+
+                        },
+                        error: function (xhr) {
+                            console.log("Error on ajax call1111 " + xhr);
+                            window.f7.hideIndicator();
+                        }
+                    });
+                });
+            }
+        },
+        components: {
             mediaItem
         }
     }
--- src/components/mediaItem.vue
+++ src/components/mediaItem.vue
@@ -1,29 +1,27 @@
 <template>
-    <div id="mediaItem">
-        <div class="msg-list" v-if="media">
-            <div class="msg-item" v-for="item in media">
-                <div class="msg-inner">
-                    <div class="tit"><a class="tit-href" href="#">{{item.title}}</a></div>
-                    <div class="user-info">
-                        <a class="a-666" href="#"><span
-                                class="name">{{item.author}}</span><span>{{item.description}}</span></a>
+        <div class="msg-inner" v-if="media">
+            <div class="tit"><a class="tit-href" href="#">{{media.title}}</a></div>
+            <div class="user-info">
+                <a class="a-666" href="#"><span
+                        class="name">{{media.author}}</span><span>{{media.description}}</span></a>
+            </div>
+            <div class="ting-box clearfix">
+                <div class="item-img dinline-l">
+                    <div class="paddingT">
                     </div>
-                    <div class="ting-box clearfix">
-                        <div class="item-img dinline-l"><a href="#"><img :src="item.imgurl" class="img-head"></a></div>
-                        <div class="item-audio dinline-l">
-                            <div class="btn-ting">
-                                <div class="btn-ting-txt"><span>¥</span><span class="price">1</span><span>听一下</span>
-                                </div>
-                            </div>
+                    <a href="#"></a>
+                    <img :src="media.imgurl" class="img-head"></div>
+                <div class="item-audio dinline-l">
+                    <div class="btn-ting">
+                        <div class="btn-ting-txt"><span>¥</span><span class="price">1</span><span>听一下</span>
                         </div>
-                    </div>
-                    <div class="answer-info">
-                        <span>答案价值 100元 </span><span>，400人听过</span><span>，200人觉得赞</span>
                     </div>
                 </div>
             </div>
+            <div class="answer-info">
+                <span>答案价值 100元 </span><span>，400人听过</span><span>，200人觉得赞</span>
+            </div>
         </div>
-    </div>
 </template>
 <script type="text/ecmascript-6">
     export default {
